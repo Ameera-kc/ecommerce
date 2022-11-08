11688d0ff35efdb05fcf1c321a319050556cc0c7
Ameera-kc <kcameera00@gmail.com>
2022-11-08 14:11:59 +0530

    index page

--- ecom/settings.py
+++ ecom/settings.py
@@ -121,7 +121,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
+STATIC_URL = "/static/"
+STATIC_FILE_ROOT = BASE_DIR / "static"
+STATICFILES_DIRS = ((BASE_DIR / "static"),)
+STATIC_ROOT = BASE_DIR / "assets"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field

2ec11993a58b41037f2fa5595568389e5f8a7913
ajin5108 <ajinjayan6@gmail.com>
2022-11-16 11:35:20 +0530

    sign-up

--- user/forms.py
+++ user/forms.py
@@ -6,9 +6,9 @@ from django.contrib.auth.forms import UserCreationForm
 
 
 class LoginRegister(UserCreationForm):
-    username = forms.CharField()
-    password1 = forms.CharField(label='Password', widget=forms.PasswordInput, )
-    password2 = forms.CharField(label='Confirm Password', widget=forms.PasswordInput, )
+    username = forms.CharField(label='username',widget=forms.TextInput(attrs={'class': 'form-control'}))
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'class': 'form-control'}) )
+    password2 = forms.CharField(label='Confirm Password', widget=forms.PasswordInput(attrs={'class': 'form-control'}) )
 
     class Meta:
         model = Login
@@ -22,7 +22,8 @@ class UserRegistration(forms.ModelForm):
             'customer_name': TextInput(attrs={'class':'form-control','name':'customer_name','placeholder':'Full Name','required':'required','autocomplete':'off',}),
             'phone_number':TextInput(attrs={'class':'form-control','name':'phone_number','placeholder':'Phone Number','required':'required','autocomplete':'off',}),
             'email': EmailInput(attrs={'class':'form-control','name':'email','placeholder':'Email','required':'required','autocomplete':'off',}),
-            'address':Textarea(attrs={'class':'form-control','name':'address','placeholder':'Full Address','required':'required','autocomplete':'off',}),  
+            'address':Textarea(attrs={'class':'form-control','name':'address','placeholder':'Full Address','required':'required','autocomplete':'off',}),
+            # 'username':Textarea(attrs={'class':'form-control','name':'username','placeholder':'username','required':'required','autocomplete':'off',}),  
             
        }
 
